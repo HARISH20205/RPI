6f99e0803561372da9bdc5d7c8477891c44e0b4e
Pradheep P <130392782+Mantissagithub@users.noreply.github.com>
2024-12-31 18:10:57 +0530

    Update prompt.py

--- src/prompt.py
+++ src/prompt.py
@@ -10,13 +10,13 @@ from nltk import word_tokenize, pos_tag, ne_chunk, sent_tokenize
 # nltk.download('averaged_perceptron_tagger_eng')
 # nltk.download('maxent_ne_chunker_tab')
 def classify_prompt_complexity(prompt):
-    nltk.download('punkt') #tokenize the given para into sentences
-    nltk.download('averaged_perceptron_tagger') #adds tags to each word in a sentence, for ex ['NLP', 'noun']
-    nltk.download('maxent_ne_chunker') #ner of the tags or words in the given sentence
-    nltk.download('words') #contains the english word corpus, which we can load here, as of our need
-    nltk.download('punkt_tab')
-    nltk.download('averaged_perceptron_tagger_eng')
-    nltk.download('maxent_ne_chunker_tab')
+    #nltk.download('punkt') #tokenize the given para into sentences
+    #nltk.download('averaged_perceptron_tagger') #adds tags to each word in a sentence, for ex ['NLP', 'noun']
+    #nltk.download('maxent_ne_chunker') #ner of the tags or words in the given sentence
+    #nltk.download('words') #contains the english word corpus, which we can load here, as of our need
+    #nltk.download('punkt_tab')
+    #nltk.download('averaged_perceptron_tagger_eng')
+    #nltk.download('maxent_ne_chunker_tab')
     # Length Classification
     length = len(prompt.split())
     if length <= 5:
@@ -83,10 +83,10 @@ def classify_prompt_complexity(prompt):
     else:
         majority_complexity = "High"
 
-    print(f"Prompt : {prompt}")
+    #print(f"Prompt : {prompt}")
     print(f"Length: {length_complexity} | NER: {ner_complexity} | Syntactic: {syntax_complexity}")
-    print(f"NER_TREE: {ner_tree}\n")
-    print(f"POS_TAG: {pos_tags}\n")
+    #print(f"NER_TREE: {ner_tree}\n")
+    #print(f"POS_TAG: {pos_tags}\n")
     print(f"Majority Complexity: {majority_complexity}\n")
     
     return majority_complexity
